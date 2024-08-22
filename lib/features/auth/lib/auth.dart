eae4932d5960d72b002207893dae1c61a4abc217
Lentscode <antonio.lentini2004@gmail.com>
2024-08-22 19:33:23 +0200

    🚀 Exposing only AuthPage from 'presentation'

--- lib/features/auth/lib/auth.dart
+++ lib/features/auth/lib/auth.dart
@@ -4,6 +4,6 @@ export "./src/config/config.dart";
 export "./src/data/data.dart";
 export "./src/logic/logic.dart";
 export "./src/models/models.dart";
-export "./src/presentation/presentation.dart";
+export "./src/presentation/presentation.dart" show AuthPage;
 export "./src/exceptions/exceptions.dart";
 export "./src/state/state.dart";
