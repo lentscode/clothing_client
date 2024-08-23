7fd72892e83706448748b5159558f07290c6a8f6
Lentscode <antonio.lentini2004@gmail.com>
2024-08-23 19:31:41 +0200

    🚧 Initial configuration of router

--- lib/common/lib/src/config/router/router.dart
+++ lib/common/lib/src/config/router/router.dart
@@ -2,6 +2,8 @@ import "package:auth/auth.dart";
 import "package:flutter/material.dart";
 import "package:go_router/go_router.dart";
 
+import "../../../common.dart";
+
 part "router.impl.dart";
 
 abstract class AppRouter {
--- lib/common/lib/src/config/router/router.impl.dart
+++ lib/common/lib/src/config/router/router.impl.dart
@@ -3,7 +3,12 @@ part of "router.dart";
 class _AppRouterImpl implements AppRouter {
   @override
   GoRouter get router => GoRouter(
-        redirect: (BuildContext context, GoRouterState state) => "/auth",
+        redirect: (BuildContext context, GoRouterState state) {
+          if (getIt.get<Auth>().user == null) {
+            return "/auth";
+          }
+          return null;
+        },
         initialLocation: "/auth",
         routes: <RouteBase>[
           _authRoute,
@@ -12,21 +17,29 @@ class _AppRouterImpl implements AppRouter {
 
   GoRoute get _authRoute => GoRoute(
         path: "/auth",
-        pageBuilder: (BuildContext context, GoRouterState state) => MaterialPage<AuthPage>(
+        pageBuilder: (BuildContext context, GoRouterState state) =>
+            MaterialPage<AuthPage>(
           child: AuthPage(
             authCubit: AuthCubit(
               loginUseCase: (String email, String password) => LoginUseCase(
                 email: email,
                 password: password,
-                auth: authGetIt.get<Auth>(),
+                auth: getIt.get<Auth>(),
               ),
-              registerUseCase: (String email, String password) => RegisterUseCase(
-                auth: authGetIt.get<Auth>(),
+              registerUseCase: (String email, String password) =>
+                  RegisterUseCase(
+                auth: getIt.get<Auth>(),
                 email: email,
                 password: password,
               ),
             ),
           ),
         ),
+      );
+
+  GoRoute get _homeRoute => GoRoute(
+        path: "/",
+        pageBuilder: (BuildContext context, GoRouterState state) =>
+            const MaterialPage(child: Scaffold()),
       );
 }
