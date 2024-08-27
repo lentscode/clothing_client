e9d73b678d1ebbf1968bd33d8126694704e4eff3
Lentscode <antonio.lentini2004@gmail.com>
2024-08-27 12:22:23 +0200

    🥅 Added exception

--- lib/features/auth/lib/src/exceptions/exceptions.dart
+++ lib/features/auth/lib/src/exceptions/exceptions.dart
@@ -5,3 +5,8 @@ class InvalidCredentialsException implements Exception {
   @override
   String toString() => "Invalid credentials";
 }
+
+class UnauthenticatedException implements Exception {
+  @override
+  String toString() => "Not authenticated";
+}
