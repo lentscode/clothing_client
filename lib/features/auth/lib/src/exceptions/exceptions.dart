2f85aaad1e66c166cc26836674cb3c49c81e70bc
Lentscode <antonio.lentini2004@gmail.com>
2024-08-29 10:57:09 +0200

    💡 Added comments

--- lib/features/auth/lib/src/exceptions/exceptions.dart
+++ lib/features/auth/lib/src/exceptions/exceptions.dart
@@ -1,11 +1,12 @@
 library;
 
-/// Throws when credentials for login or registration are incorrect.
+/// Thrown when credentials for login or registration are incorrect.
 class InvalidCredentialsException implements Exception {
   @override
   String toString() => "Invalid credentials";
 }
 
+/// Thrown when a request fails because the user is unauthenticated.
 class UnauthenticatedException implements Exception {
   @override
   String toString() => "Not authenticated";
